7112ffe0419be1b01df8042b5f0f6d387c522668
AvdzhiV <avdjan.vg@aobfg.ru>
2024-09-12 10:29:30 +0300

    added pkg

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,56 +1,12 @@
 package main
 
 import (
-	"github.com/AvdzhiV/urlShort/configs"
-	"github.com/go-chi/chi/v5"
-	"io"
-	"math/rand"
 	"net/http"
 	"strconv"
+
+	"github.com/AvdzhiV/urlShort/configs"
+	"github.com/go-chi/chi/v5"
 )
-
-var urlMap = make(map[string]string)
-
-func generateShortURL() string {
-	letterBytes := "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
-	b := make([]byte, 6)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
-	}
-	return string(b)
-}
-
-func shorterHandlerPost(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			return
-		}
-		origURL := string(body)
-		shortURL := generateShortURL()
-
-		urlMap[shortURL] = origURL
-
-		fullShortURL := configs.ParseParts().BaseURL + "/" + shortURL
-
-		w.WriteHeader(http.StatusCreated)
-		w.Header().Set("Content-Type", "text/plain")
-		w.Write([]byte(fullShortURL))
-	}
-}
-
-func shorterHandlerGet(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		shortURL := chi.URLParam(r, "shortURL")
-
-		origURL, exists := urlMap[shortURL]
-		if !exists {
-			http.Error(w, "URL not found", http.StatusNotFound)
-			return
-		}
-		http.Redirect(w, r, origURL, http.StatusTemporaryRedirect)
-	}
-}
 
 func main() {
 	cfg := configs.ParseParts()
---
+++ pkg/generateURL.go
@@ -0,0 +1,12 @@
+package pkg
+
+import "math/rand"
+
+func generateShortURL() string {
+	letterBytes := "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+	b := make([]byte, 6)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
